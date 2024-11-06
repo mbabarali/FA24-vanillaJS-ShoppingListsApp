8b1b37adc69c77a8c5efe8d04c91c02d519dc5a6
Muhammad Babar Ali <babar.magsi@gmail.com>
2024-11-06 06:54:52 +0500

    Shift: Register events - mark, seek and delete

---
+++ index.js
@@ -0,0 +1,45 @@
+// ==============================================================
+// Start: Register Event Handlers on Buttons in Items
+// ==============================================================
+console.log("==============================================");
+function shiftToMarkEventHandler(e) {
+  console.log("[ITEM-MARKED]");
+  console.log("target", e.target);
+  console.log("parentElement", e.target.parentElement);
+}
+
+// --------------------------------------------------
+function shiftToSeekEventHandler(e) {
+  console.log("[ITEM-RESTORED]");
+  console.log("target", e.target);
+  console.log("parentElement", e.target.parentElement);
+}
+
+// --------------------------------------------------
+function deleteEventHandler(e) {
+  console.log("[ITEM-DELETED]");
+  console.log("target", e.target);
+  console.log("parentElement", e.target.parentElement);
+}
+
+console.log("==============================================");
+const markBtnAll = document.querySelectorAll(".mark-btn");
+markBtnAll.forEach((item) => {
+  console.log(item);
+  // item.onclick = shiftToMarkEventHandler; // Bad Practice
+  item.addEventListener("click", shiftToMarkEventHandler);
+});
+
+// --------------------------------------------------
+const seekBtnAll = document.querySelectorAll(".seek-btn");
+seekBtnAll.forEach((item) => {
+  console.log(item);
+  item.addEventListener("click", shiftToSeekEventHandler);
+});
+
+// --------------------------------------------------
+const deleteBtnAll = document.querySelectorAll(".delete-btn");
+deleteBtnAll.forEach((item) => {
+  console.log(item);
+  item.addEventListener("click", deleteEventHandler);
+});
