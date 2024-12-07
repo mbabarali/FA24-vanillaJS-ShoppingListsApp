11f701717d601909c293f67b1136c4baead95e12
Muhammad Babar Ali <144947128+mbabarali@users.noreply.github.com>
2024-12-07 13:40:37 +0500

    Shift: Handle events - mark, seek and delete

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ function shiftToMarkEventHandler(e) {
   console.log("[ITEM-MARKED]");
   console.log("target", e.target);
   console.log("parentElement", e.target.parentElement);
+
+  // Identify item to move
+  const itemToMove = e.target.parentElement;
+
+  // Find destination list
+  const sourceListIdArray = e.target.parentElement.parentElement.id.split("-");
+  const destinationId = sourceListIdArray[0] + "-mark-list";
+  const destinationList = document.querySelector(`#${destinationId}`);
+
+  console.log("itemToMove", itemToMove, "--> Type:", typeof itemToMove);
+  console.log(
+    "sourceListIdArray",
+    sourceListIdArray,
+    "--> Type:",
+    typeof sourceListIdArray
+  );
+
+  // Update item
+  let shiftBtn = itemToMove.querySelector("Button");
+  shiftBtn.removeEventListener("click", shiftToMarkEventHandler);
+  shiftBtn.addEventListener("click", shiftToSeekEventHandler);
+  shiftBtn.innerHTML = "&#8634;";
+  shiftBtn.classList = shiftBtn.classList.value.replace("mark", "seek");
+
+  console.log("shiftBtn", shiftBtn, "--> Type:", typeof shiftBtn);
+
+  // Move item to destination list
+  destinationList.appendChild(itemToMove);
+
+  console.log(
+    "destinationId",
+    destinationId,
+    "--> Type:",
+    typeof destinationId
+  );
+  console.log(
+    "destinationList",
+    destinationList,
+    "--> Type:",
+    typeof destinationList
+  );
 }
 
 // --------------------------------------------------
@@ -13,6 +54,47 @@ function shiftToSeekEventHandler(e) {
   console.log("[ITEM-RESTORED]");
   console.log("target", e.target);
   console.log("parentElement", e.target.parentElement);
+
+  // Identify item to move
+  const itemToMove = e.target.parentElement;
+
+  // Find destination list
+  const sourceListIdArray = e.target.parentElement.parentElement.id.split("-");
+  const destinationId = sourceListIdArray[0] + "-seek-list"; //-+
+  const destinationList = document.querySelector(`#${destinationId}`);
+
+  console.log("itemToMove", itemToMove, "--> Type:", typeof itemToMove);
+  console.log(
+    "sourceListIdArray",
+    sourceListIdArray,
+    "--> Type:",
+    typeof sourceListIdArray
+  );
+
+  // Update item
+  let shiftBtn = itemToMove.querySelector("Button");
+  shiftBtn.removeEventListener("click", shiftToSeekEventHandler); //-+
+  shiftBtn.addEventListener("click", shiftToMarkEventHandler); //-+
+  shiftBtn.innerHTML = "&check;"; //-+
+  shiftBtn.classList = shiftBtn.classList.value.replace("seek", "mark"); //-+
+
+  console.log("shiftBtn", shiftBtn, "--> Type:", typeof shiftBtn);
+
+  // Move item to destination list
+  destinationList.appendChild(itemToMove);
+
+  console.log(
+    "destinationId",
+    destinationId,
+    "--> Type:",
+    typeof destinationId
+  );
+  console.log(
+    "destinationList",
+    destinationList,
+    "--> Type:",
+    typeof destinationList
+  );
 }
 
 // --------------------------------------------------
@@ -20,6 +102,13 @@ function deleteEventHandler(e) {
   console.log("[ITEM-DELETED]");
   console.log("target", e.target);
   console.log("parentElement", e.target.parentElement);
+
+  // // Identify Item to delete
+  // const itemToDelete = e.target.parentElement;
+  // console.log("itemToDelete", itemToDelete, "--> Type:", typeof itemToDelete);
+
+  // Delete item
+  e.target.parentElement.remove();
 }
 
 console.log("==============================================");
