3c761647256ee3bd329381fbea9b26f504d3ff07
Muhammad Babar Ali <144947128+mbabarali@users.noreply.github.com>
2024-12-11 12:59:48 +0500

    State: Initialize and update state using Constructor Function

--- index.js
+++ index.js
@@ -83,6 +83,50 @@ const INIIAL_SHOPPING_LIST = [
 // console.log("INIIAL_SHOPPING_LIST", INIIAL_SHOPPING_LIST);
 
 // State must be 1) Accessible within all app 2) Singleton
+function AppState(initialState) {
+  const internalState = [...initialState]; // Private but not a singleton state
+
+  // this.state = [...internalState]; // Initilization only
+  this.state = function () {
+    return [...internalState];
+  };
+
+  this.updateState = function (id, updates, isNew = false) {
+    let item = {};
+
+    // New Item
+    if (isNew) {
+      id && (item.id = id);
+      item.title = updates?.title ?? "<EMPTY>";
+      item.status = updates?.status ?? "seek";
+      item.category = updates?.category ?? "grocery";
+      item.trash = updates?.trash ?? false;
+      item.createDate = new Date().toISOString().slice(0, 10);
+      internalState.push(item);
+      return;
+    }
+
+    // Existing Item
+    item = internalState.find((item) => {
+      console.log(item);
+      return item.id === id;
+    });
+
+    item || alert("[MISMATCH] Entry vs. item: Can not update item.");
+
+    updates.title && (item.title = updates.title);
+    updates.status && (item.status = updates.status);
+    updates.category && (item.category = updates.category);
+    updates.trash && (item.trash = updates.trash);
+
+    console.log(item);
+    console.log(internalState);
+
+    return;
+  };
+}
+
+/*
 const appState = (function (initialState) {
   const internalState = [...initialState];
 
@@ -126,7 +170,9 @@ const appState = (function (initialState) {
     },
   };
 })(INIIAL_SHOPPING_LIST);
+*/
 
+const appState = new AppState(INIIAL_SHOPPING_LIST);
 console.log("state", appState.state);
 
 /*
